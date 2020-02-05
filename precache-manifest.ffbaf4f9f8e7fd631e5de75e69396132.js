63d26603326efdb1ca761ee3f9fea11918620124
Ghoster23 <mmpcp23@gmail.com>
2020-02-05 02:03:50 +0000

    Updates

---
+++ precache-manifest.ffbaf4f9f8e7fd631e5de75e69396132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c693efa1a0dab3c2c65081e071f18cc",
+    "url": "/PokemonMacrosJoyConDroid/index.html"
+  },
+  {
+    "revision": "ff69b6feb3098be63bcd",
+    "url": "/PokemonMacrosJoyConDroid/static/css/main.b897dbaf.chunk.css"
+  },
+  {
+    "revision": "68e3e79f325e5788c2db",
+    "url": "/PokemonMacrosJoyConDroid/static/js/2.0ac95d66.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/PokemonMacrosJoyConDroid/static/js/2.0ac95d66.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff69b6feb3098be63bcd",
+    "url": "/PokemonMacrosJoyConDroid/static/js/main.e8989af7.chunk.js"
+  },
+  {
+    "revision": "8d0d80a8c1dbdb9c4fd6",
+    "url": "/PokemonMacrosJoyConDroid/static/js/runtime-main.ad906206.js"
+  }
+]);
