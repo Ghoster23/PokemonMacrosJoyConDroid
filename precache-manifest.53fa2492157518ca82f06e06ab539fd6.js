e28abb0c6dc01ab943fbd549f6750b9ff5886e26
Manel <mmpcp23@gmail.com>
2020-03-25 10:39:13 +0000

    Updates

--- precache-manifest.53fa2492157518ca82f06e06ab539fd6.js
+++ precache-manifest.53fa2492157518ca82f06e06ab539fd6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eaaf39981f6abcde6ea9392b340b6691",
+    "revision": "e519fc8943534aadf6833b106fe04575",
     "url": "/PokemonMacrosJoyConDroid/index.html"
   },
   {
-    "revision": "225fcc26dfe98b8321fb",
+    "revision": "634c278654aa04b98733",
     "url": "/PokemonMacrosJoyConDroid/static/css/main.acbdd099.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/PokemonMacrosJoyConDroid/static/js/2.0ac95d66.chunk.js.LICENSE"
   },
   {
-    "revision": "225fcc26dfe98b8321fb",
-    "url": "/PokemonMacrosJoyConDroid/static/js/main.0e226c09.chunk.js"
+    "revision": "634c278654aa04b98733",
+    "url": "/PokemonMacrosJoyConDroid/static/js/main.7670626e.chunk.js"
   },
   {
     "revision": "8d0d80a8c1dbdb9c4fd6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PokemonMacrosJoyConDroid/precache-manifest.75242225e8a08548c998ac9983f834c6.js"
+  "/PokemonMacrosJoyConDroid/precache-manifest.53fa2492157518ca82f06e06ab539fd6.js"
 );
 
 self.addEventListener('message', (event) => {
